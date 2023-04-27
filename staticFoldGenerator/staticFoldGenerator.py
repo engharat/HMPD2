0bca5d8375af4c698d5227a09cc1d7029d62397a
beppe2hd <beppe2hd@gmail.com>
2023-04-27 15:05:53 +0200

    folding for possible microplastic has been created

--- staticFoldGenerator/staticFoldGenerator.py
+++ staticFoldGenerator/staticFoldGenerator.py
@@ -7,11 +7,11 @@ kfold = KFold(n_splits=5, shuffle=True)
 for channel in ['A', 'P', 'R']:
 
     #a =glob(f"/Users/beppe2hd/Data/Microplastiche/HMPD-Gen/images/*_{channel}.bmp")
-    df = pd.read_csv("/Users/beppe2hd/Data/Microplastiche/HMPD-Gen/gt.csv")#  pd.DataFrame(a)
+    df = pd.read_csv("/Users/beppe2hd/Data/Microplastiche/HMPD-Gen/gtPossible.csv")#  pd.DataFrame(a)
 
     for i, indexes in enumerate(kfold.split(df)):
         dfTrain = df.iloc[indexes[0]]
         dfTest = df.iloc[indexes[1]]
-        dfTrain.to_csv(f"./{channel}/train_{i}.csv", index=False, header=False)
-        dfTest.to_csv(f"./{channel}/test_{i}.csv", index=False, header=False)
+        dfTrain.to_csv(f"./GT_P/{channel}/train_{i}.csv", index=False, header=False)
+        dfTest.to_csv(f"./GT_P/{channel}/test_{i}.csv", index=False, header=False)
 
